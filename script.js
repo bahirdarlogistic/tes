0374220d3ea0046949c997d88f7b422d9c9059e8
bahirdarlogistic <yohannesbiazn1@gmail.com>
2025-01-10 09:28:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const params=new URLSearchParams(window.location.search);
+const value1=params.get('param1');
+const value2=params.get('param2');
+const value3=params.get('param3');
+const value4=params.get('param4');
+document.getElementById("targa").innerText='12345';
+document.getElementById("name").innerText='ማንደፍሮ ወንዴ';
+document.getElementById("kebele").innerText='13';
+document.getElementById("phone").innerText='964677018';
+if(params.has('param1')){
+  document.getElementById("targa").innerText=value1;
+  document.getElementById("name").innerText=value2;
+  document.getElementById("kebele").innerText=value3;
+  document.getElementById("phone").innerText=value4;
+}
+
+scrollTo(150,9100);
+  
